57a8ca5fa5212f61f0745f3fe4f6c26870caf0c7
czu-zhang <zheqingzhang@163.com>
2025-07-19 01:20:44 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 #coding:utf-8
 from ultralytics import YOLO
-# # 模型推理
+# # model inference
 model = YOLO('runs/detect/train120/weights/best.pt')
 model.predict(source='datasets/test/stain.jpg', **{'save': True})
